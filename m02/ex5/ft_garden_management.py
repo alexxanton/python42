a11eafe98a2758b99ccd278a513b9c3b51b605a0
Alex Anton Aranda <aanton-a@cbr2s4.42barcelona.com>
2026-02-19 16:10:18 +0100

    finished ex5

--- m02/ex5/ft_garden_management.py
+++ m02/ex5/ft_garden_management.py
@@ -16,6 +16,12 @@ class WaterError(GardenError):
         GardenError.__init__(self, message)
 
 
+class SunlightError(GardenError):
+    """Exception for sunlight errors"""
+    def __init__(self, message: str = "Sunlight Error") -> None:
+        GardenError.__init__(self, message)
+
+
 class GardenManager:
     """Contains the plants and methods to take care of them"""
     def __init__(self) -> None:
@@ -47,14 +53,44 @@ class GardenManager:
         finally:
             print("Closing watering system (cleanup)")
 
-    def check_plants_health(self) -> None:
+    def check_plant_health(self, plant: tuple[str, int, int]) -> None:
+        """Checks if the values are valid"""
+        plant_name, water_level, sunlight_hours = plant
+        if not plant_name:
+            raise PlantError("Plant name cannot be empty!")
+
+        if water_level > 10:
+            raise WaterError(f"Water level {water_level} is too high (max 10)")
+        if water_level < 1:
+            raise WaterError(f"Water level {water_level} is too low (min 1)")
+
+        if sunlight_hours > 12:
+            raise SunlightError(
+                f"Sunlight hours {sunlight_hours} is too high (max 12)"
+            )
+        if sunlight_hours < 2:
+            raise SunlightError(
+                f"Sunlight hours {sunlight_hours} is too low (min 2)"
+            )
+
+    def check_plants(self) -> None:
         """Checks the health of a plant, displays errors for invalid values"""
         print("\nChecking plant health...")
         for plant, water, sun in self.plants:
             try:
+                self.check_plant_health((plant, water, sun))
                 print(f"{plant}: healthy (water: {water}, sun: {sun})")
-            except PlantError as e:
-                print(f"Error adding plant: {e}")
+            except (PlantError, WaterError, SunlightError) as e:
+                print(f"Error: {e}")
+
+    def error_recovery(self) -> None:
+        print("\nTesting error recovery...")
+        try:
+            raise GardenError("Not enough water in tank")
+        except GardenError as e:
+            print("Caught GardenError:", e)
+        finally:
+            print("System recovered and continuing...")
 
 
 def test_garden_management() -> None:
@@ -67,7 +103,8 @@ def test_garden_management() -> None:
     manager = GardenManager()
     manager.add_plants(plants)
     manager.water_plants()
-    manager.check_plants_health()
+    manager.check_plants()
+    manager.error_recovery()
     print("\nGarden management system test complete!")
 
 
