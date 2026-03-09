aef6305a5d47855f4c77f33295cbc4a9d8e07d98
Alex Anton Aranda <aanton-a@cbr1s4.42barcelona.com>
2026-03-09 11:58:06 +0100

    removed prev versions from m06

--- m06/v4/alchemy/transmutation/__init__.py
+++
@@ -1,9 +0,0 @@
-__all__ = [
-    "lead_to_gold",
-    "stone_to_gem",
-    "philosophers_stone",
-    "elixir_of_life",
-]
-
-from .basic import lead_to_gold, stone_to_gem
-from .advanced import philosophers_stone, elixir_of_life
